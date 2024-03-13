ac205d1a39d36204292479ea08f792a93871d91c
NainaMhrzn <nainamaharjan99@gmail.com>
2024-03-13 11:29:37 +0545

    Initial Commit

---
+++ CarouselSwiftUI/CarouselSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselSwiftUIApp.swift
+//  CarouselSwiftUI
+//
+//  Created by Naina Maharjan on 13/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct CarouselSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
